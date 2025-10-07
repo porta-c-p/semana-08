291719db97b11e20972bccd0bc461cf2fd9e6fcd
Richard Chavez Palma <richard.chavez@tecsup.edu.pe>
2025-10-07 15:04:53 -0500

    feat: implementacion de dotenv y .gitignore

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const http = require('http');
+require('dotenv').config() // Línea 1: Carga las variables del archivo .env
+const http = require('http')
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
+function requestController(req, res){
+  // Aquí se manejan las peticiones HTTP
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('¡Hola desde Node.js en produccion!');
-});
+  res.end('Bienvenidos al curso, ejecutandose en el puerto ' + process.env.PORT);
+  console.log('Bienvenidos al curso')
+}
 
-server.listen(port, hostname, () => {
-  console.log(`Servidor corriendo en http://${hostname}:${port}/`);
-});
+const server = http.createServer(requestController)
+
+const PORT = process.env.PORT // Línea 10: Obtiene el puerto de las variables de entorno
+
+server.listen(PORT, function(){
+  console.log("Aplicacion corriendo en: " + PORT)
+})
